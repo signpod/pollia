ec58f3954bf2a4701650ebd7013de9801fe41bbb
lurgi <wahoo9040@naver.com>
2025-09-02 13:40:35 +0900

    feat: 기본적은 Shadcn 컴포넌트 설치

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -3,6 +3,17 @@ const nextConfig = {
   experimental: {
     optimizePackageImports: ["@radix-ui/react-dialog"],
   },
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
