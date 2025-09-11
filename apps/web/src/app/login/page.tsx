f0b0f53a1e42a312893374261619d2595a1e3391
TaehunLee <85233397+Todari@users.noreply.github.com>
2025-09-12 08:25:23 +0900

    fix: 카카오 로그인 버튼 상호작용 개선 및 SVG 속성 오류 수정 (#11)
    * feat: 카카오 로그인 버튼 컴포넌트 추가 및 로그인 페이지에서 기존 버튼 대체
    
    * feat: 카카오 로그인 버튼에 툴팁 데이터 속성 추가 및 SVG 속성 수정
    
    * refactor: 로그인 페이지에서 사용하지 않는 KakaoIcon 및 cn 임포트 제거

--- apps/web/src/app/login/page.tsx
+++ apps/web/src/app/login/page.tsx
@@ -5,8 +5,6 @@ import { useSearchParams } from "next/navigation";
 import { API_BASE_URL } from "../../constants/config";
 import { BottomCTALayout, Button, KakaoLoginButton, Tooltip } from "@repo/ui/components";
 import { OnboardingCarousel } from "./OnboardingCarousel";
-import KakaoIcon from "@public/svgs/kakao-icon.svg";
-import { cn } from "@repo/ui/lib";
 
 export default function LoginPage() {
   const searchParams = useSearchParams();
@@ -31,7 +29,7 @@ export default function LoginPage() {
           >
             ⚡️ 3초만에 시작하기
           </Tooltip>
-          <KakaoLoginButton onClick={handleKakaoLogin} />
+          <KakaoLoginButton data-tooltip-id="kakao-login-tooltip" onClick={handleKakaoLogin} />
         </div>
       </BottomCTALayout.CTA>
     </>
