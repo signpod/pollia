7080f1da1416f2f2f0a6d40fa1630078ed906c73
lurgi <wahoo9040@naver.com>
2025-09-18 16:13:53 +0900

    feat: CandidateSelector 컴포넌트 구현

--- apps/web/src/app/create/CandidateSelector.tsx
+++ apps/web/src/app/create/CandidateSelector.tsx
@@ -1,3 +1,45 @@
+import PollCandidate from "@/components/poll/PollCandidate";
+import { useMultipleCandidate } from "@/hooks/poll/useMultipleCandidate";
+import { Button, Typo } from "@repo/ui/components";
+import { PlusIcon } from "lucide-react";
+
 export default function CandidateSelector() {
-  return <div>CandidateSelector</div>;
+  const { candidates, updateCandidate, addCandidate } = useMultipleCandidate();
+
+  return (
+    <>
+      <div className="flex items-center justify-between">
+        <div className="flex items-center gap-1">
+          <Typo.SubTitle size="large">투표 항목</Typo.SubTitle>
+          <span className="text-red-500">*</span>
+        </div>
+        <Typo.Body size="small" className="text-zinc-400">
+          {candidates.length}/10
+        </Typo.Body>
+      </div>
+
+      {candidates.map((candidate) => {
+        const handleNameChange = (name: string) => {
+          updateCandidate(candidate.id, { name });
+        };
+
+        return (
+          <PollCandidate
+            key={candidate.id}
+            onNameChange={handleNameChange}
+            {...candidate}
+          />
+        );
+      })}
+
+      {/* TODO: 버튼 수정이루어지면 수정*/}
+      <Button
+        variant="secondary"
+        leftIcon={<PlusIcon />}
+        onClick={addCandidate}
+      >
+        <Typo.ButtonText size="large">항목 추가하기</Typo.ButtonText>
+      </Button>
+    </>
+  );
 }
