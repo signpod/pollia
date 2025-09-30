4ada77a25f19ac0b0efbc2871c7d8fe4ab7391f4
lurgi <wahoo9040@naver.com>
2025-10-01 08:30:41 +0900

    feat: Poll 페이지에 prefetch 로직 추가 및 loginModal 렌더링 위한 세팅

---
+++ apps/web/src/app/poll/[id]/PollHeader.tsx
@@ -0,0 +1,13 @@
+import { FixedTopLayout, IconButton } from "@repo/ui/components";
+import { MoreVertical, X } from "lucide-react";
+
+export function PollHeader() {
+  return (
+    <FixedTopLayout.Content className="bg-zinc-50">
+      <div className="flex items-center justify-between px-1">
+        <IconButton icon={X} className="size-12" />
+        <IconButton icon={MoreVertical} className="size-12" />
+      </div>
+    </FixedTopLayout.Content>
+  );
+}
