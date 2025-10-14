d3d0045f400dcd683170dc16bdac7013bf3f3377
lurgi <wahoo9040@naver.com>
2025-10-14 09:41:52 +0900

    chore: 스토리북 svgr 세팅

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -20,10 +20,27 @@ const config: StorybookConfig = {
     if (config.resolve) {
       config.resolve.alias = {
         ...config.resolve.alias,
-        "@": join(__dirname, "../web/src"),
-        "@public": join(__dirname, "../web/public"),
+        "@": join(__dirname, "../../web/src"),
+        "@public": join(__dirname, "../../web/public"),
       };
     }
+
+    // SVG를 React 컴포넌트로 import할 수 있도록 설정
+    const fileLoaderRule = config.module?.rules?.find((rule) => {
+      if (typeof rule !== "object" || !rule) return false;
+      if (!("test" in rule)) return false;
+      return rule.test instanceof RegExp && rule.test.test(".svg");
+    });
+
+    if (fileLoaderRule && typeof fileLoaderRule === "object") {
+      fileLoaderRule.exclude = /\.svg$/;
+    }
+
+    config.module?.rules?.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
     return config;
   },
 };
