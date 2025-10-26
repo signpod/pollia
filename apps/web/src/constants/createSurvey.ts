35fd0d3169cb6fc75e3e8903ffd72ef96f61e626
lurgi <wahoo9040@naver.com>
2025-10-27 07:16:18 +0900

    feat: survey 도메인에 대한 Type 전역 상태 및 Step 상태 구현

---
+++ apps/web/src/constants/createSurvey.ts
@@ -0,0 +1,82 @@
+import React from "react";
+import { StepConfig } from "@repo/ui/components";
+import { ChevronLeft, LucideIcon, X } from "lucide-react";
+import { SurveyType } from "@/types/domain/survey";
+
+export interface ExtendedStepConfig extends StepConfig {
+  description?: string;
+  header: {
+    action: () => void;
+    icon: LucideIcon | React.ComponentType<React.SVGProps<SVGSVGElement>>;
+  };
+  content: () => React.ReactNode;
+}
+
+type CreateSurveyStep = [StepConfig, StepConfig];
+
+export const CREATE_SURVEY_STEPS: CreateSurveyStep = [
+  {
+    id: "type",
+    title: "질문의 유형을 선택해주세요!",
+    canGoNext: true,
+    canGoBack: false,
+  },
+  {
+    id: "content",
+    title: "질문의 내용을 작성해주세요!",
+    canGoNext: true,
+    canGoBack: true,
+  },
+];
+
+interface StepComponents {
+  TypeStep: React.ComponentType;
+  EitherOrInfoStep: React.ComponentType;
+  MultipleChoiceInfoStep: React.ComponentType;
+  ScaleInfoStep: React.ComponentType;
+  SubjectiveInfoStep: React.ComponentType;
+}
+
+interface CreateStepConfigsProps {
+  router: { back: () => void };
+  goBack: () => void;
+  surveyType: SurveyType;
+  stepComponents: StepComponents;
+}
+
+export const createStepConfigs = ({
+  router,
+  goBack,
+  surveyType,
+  stepComponents,
+}: CreateStepConfigsProps): ExtendedStepConfig[] => [
+  {
+    ...CREATE_SURVEY_STEPS[0],
+    description: "원하는 질문 방식을 골라주세요",
+    header: {
+      action: () => router.back(),
+      icon: X,
+    },
+    content: () => React.createElement(stepComponents.TypeStep),
+  },
+  {
+    ...CREATE_SURVEY_STEPS[1],
+    description: undefined,
+    header: {
+      action: goBack,
+      icon: ChevronLeft,
+    },
+    content: () => {
+      switch (surveyType) {
+        case SurveyType.EITHER_OR:
+          return React.createElement(stepComponents.EitherOrInfoStep);
+        case SurveyType.MULTIPLE_CHOICE:
+          return React.createElement(stepComponents.MultipleChoiceInfoStep);
+        case SurveyType.SCALE:
+          return React.createElement(stepComponents.ScaleInfoStep);
+        case SurveyType.SUBJECTIVE:
+          return React.createElement(stepComponents.SubjectiveInfoStep);
+      }
+    },
+  },
+];
---
+++ apps/web/src/constants/survey.ts
@@ -0,0 +1,8 @@
+import { SurveyType } from "@/types/domain/survey";
+
+export const SURVEY_TYPES: SurveyType[] = [
+  SurveyType.EITHER_OR,
+  SurveyType.MULTIPLE_CHOICE,
+  SurveyType.SCALE,
+  SurveyType.SUBJECTIVE,
+];
