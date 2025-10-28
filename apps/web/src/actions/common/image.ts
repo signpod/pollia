3fa8bf6e5e7973c4ab9bac0a50ebd2a34d1c3404
lurgi <wahoo9040@naver.com>
2025-10-28 10:18:47 +0900

    refactor: action common 함수로 리팩터링

--- apps/web/src/actions/common/image.ts
+++ apps/web/src/actions/common/image.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { requireAuth } from "@/actions/auth";
+import { requireAuth } from "@/actions/common/auth";
 import { createClient as createServerSupabaseClient } from "@/database/utils/supabase/server";
 import prisma from "@/database/utils/prisma/client";
 import { FileStatus } from "@prisma/client";
--- apps/web/src/actions/poll/bookmark.ts
+++ apps/web/src/actions/poll/bookmark.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { requireAuth } from "@/actions/auth";
+import { requireAuth } from "@/actions/common/auth";
 import prisma from "@/database/utils/prisma/client";
 
 export async function bookmarkPoll(pollId: string) {
