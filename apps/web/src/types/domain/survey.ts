06b39d0c9ee1d6094ac25d3b989f55bb25b3dc46
choi river <130737187+0jenn0@users.noreply.github.com>
2025-11-04 09:20:53 +0900

    refactor: 타입 분리

--- apps/web/src/types/domain/survey.ts
+++ apps/web/src/types/domain/survey.ts
@@ -1,5 +1,9 @@
-import { SurveyQuestion, SurveyQuestionType } from "@prisma/client";
+import { Survey, SurveyQuestion, SurveyQuestionType } from "@prisma/client";
 
 export { SurveyQuestionType };
 
 export type SurveyQuestionSummary = Pick<SurveyQuestion, "id" | "title" | "type" | "createdAt">;
+export type SurveySummary = Pick<
+  Survey,
+  "id" | "title" | "description" | "imageUrl" | "createdAt" | "updatedAt"
+>;
