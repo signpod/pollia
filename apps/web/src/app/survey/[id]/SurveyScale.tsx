c74a05330c772af4b955e60a4732cd92761213fa
choi river <130737187+0jenn0@users.noreply.github.com>
2025-11-11 10:35:14 +0900

    refactor: 질문 페이지의 layout 컴포넌트로 분리

--- apps/web/src/app/survey/[id]/SurveyScale.tsx
+++ apps/web/src/app/survey/[id]/SurveyScale.tsx
@@ -1,16 +1,8 @@
-import { scaleValueAtom } from "@/atoms/survey/question/scaleInfoAtoms";
-import {
-  ButtonV2,
-  FixedBottomLayout,
-  FixedTopLayout,
-  ProgressBar,
-  Typo,
-} from "@repo/ui/components";
+import { scaleResponseAtom } from "@/atoms/survey/question/response/scaleAtoms";
 import { useAtom } from "jotai";
-import { ChevronLeftIcon } from "lucide-react";
-import Image from "next/image";
 import { useState } from "react";
 import { SurveyLikertScale } from "./components/SurveyLikertScale";
+import { SurveyQuestionLayout } from "./components/SurveyQuestionLayout";
 
 const mockData = {
   title: "설문조사 제목입니다. 최대 20자까지 가능합니다.",
@@ -26,55 +18,20 @@ export function SurveyScale() {
   const isFirstQuestion = order === 1;
 
   return (
-    <main>
-      <FixedBottomLayout>
-        <FixedTopLayout>
-          <FixedTopLayout.Content className="pt-3">
-            <ProgressBar value={(order / totalQuestionCount) * 100 || 0} />
-          </FixedTopLayout.Content>
-        </FixedTopLayout>
-        <div className="space-y-8 mt-9 px-5 pb-5">
-          <section className="pt-5 space-y-2">
-            <Typo.MainTitle size="medium">{title}</Typo.MainTitle>
-            <Typo.Body size="large" className="text-sub">
-              {description}
-            </Typo.Body>
-            {imageUrl && (
-              <figure className="relative aspect-[3/2] overflow-hidden rounded-sm">
-                <Image src={imageUrl} alt={title} fill className="object-cover" />
-              </figure>
-            )}
-          </section>
-
-          <SurveyLikertScale value={scaleValue} onChange={handleScaleValueChange}>
-            <SurveyLikertScale.ScaleGuide labels={SURVEY_LIKERT_SCALE_TEXT} />
-            <SurveyLikertScale.Thumb value={scaleValue} />
-          </SurveyLikertScale>
-        </div>
-        <FixedBottomLayout.Content className="px-5 py-3">
-          <nav className="flex gap-2">
-            <ButtonV2
-              variant="secondary"
-              size="large"
-              className="w-fit aspect-square"
-              disabled={isFirstQuestion}
-            >
-              <ChevronLeftIcon className="size-6" />
-            </ButtonV2>
-            <ButtonV2
-              variant="primary"
-              size="large"
-              className="flex-1 flex"
-              disabled={!isScaleValueChanged}
-            >
-              <Typo.ButtonText size="large" className="flex justify-center w-full">
-                다음
-              </Typo.ButtonText>
-            </ButtonV2>
-          </nav>
-        </FixedBottomLayout.Content>
-      </FixedBottomLayout>
-    </main>
+    <SurveyQuestionLayout
+      currentOrder={order}
+      totalQuestionCount={totalQuestionCount}
+      title={title}
+      description={description}
+      imageUrl={imageUrl}
+      isFirstQuestion={isFirstQuestion}
+      isNextDisabled={!isScaleValueChanged}
+    >
+      <SurveyLikertScale value={scaleValue} onChange={handleScaleValueChange}>
+        <SurveyLikertScale.ScaleGuide labels={SURVEY_LIKERT_SCALE_TEXT} />
+        <SurveyLikertScale.Thumb value={scaleValue} />
+      </SurveyLikertScale>
+    </SurveyQuestionLayout>
   );
 }
 
@@ -82,7 +39,7 @@ const SURVEY_LIKERT_SCALE_TEXT = ["매우 불만족", "불만족", "보통", "�
 
 function useSurveyScaleValue() {
   const [isScaleValueChanged, setIsScaleValueChanged] = useState(false);
-  const [scaleValue, setScaleValue] = useAtom(scaleValueAtom);
+  const [scaleValue, setScaleValue] = useAtom(scaleResponseAtom);
 
   const handleScaleValueChange = (value: number) => {
     if (!isScaleValueChanged) {
