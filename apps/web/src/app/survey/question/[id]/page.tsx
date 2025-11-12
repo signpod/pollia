06a591d6f11ad0fb7ca2b0959d15c6984c1d1ef9
choi river <130737187+0jenn0@users.noreply.github.com>
2025-11-13 08:51:07 +0900

    chore: 개별 질문은 question 경로로 이동

--- apps/web/src/app/survey/question/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-interface SurveyPageProps {
-  params: Promise<{ id: string }>;
-}
-
-export default async function SurveyPage({ params }: SurveyPageProps) {
-  const { id } = await params;
-
-  return <div>Survey Question {id}</div>;
-}
