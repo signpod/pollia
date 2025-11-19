71c4ad5c7d75cec4f121751d906fb6315571d7b7
choi river <130737187+0jenn0@users.noreply.github.com>
2025-11-19 14:46:51 +0900

    refactor: 상수를 module로 분리
    - 이 페이지 외에 다른 페이지에서도 쓰이기에 constants 폴더로 따로 분리함

--- apps/web/src/app/survey/[id]/question/components/SurveyQuestionTemplate.tsx
+++ apps/web/src/app/survey/[id]/question/components/SurveyQuestionTemplate.tsx
@@ -1,4 +1,5 @@
 import { toast } from "@/components/common/Toast";
+import { SURVEY_TOAST_MESSAGE } from "@/constants/surveyMessages";
 import {
   ButtonV2,
   FixedBottomLayout,
@@ -9,25 +10,6 @@ import {
 import { ChevronLeftIcon } from "lucide-react";
 import Image from "next/image";
 import { type PropsWithChildren, useEffect } from "react";
-
-const TOAST_MESSAGE = {
-  first: {
-    id: "survey-toast-first",
-    message: "솔직하게 답변해주세요! 👀",
-  },
-  half: {
-    id: "survey-toast-half",
-    message: "벌써 반이나 응답하셨어요! 조금만 더 힘내요😎",
-  },
-  final: {
-    id: "survey-toast-final",
-    message: "드디어 마지막 질문이에요! 🎁",
-  },
-  error: {
-    id: "survey-toast-error",
-    message: "네트워크에 문제가 발생했어요.\n잠시후 다시 시도해주세요.",
-  },
-};
 
 let hasShownHalfToast = false;
 
@@ -64,17 +46,19 @@ export function SurveyQuestionTemplate({
     const isHalfway = progressValue >= 50 && !hasShownHalfToast;
 
     if (isFirstQuestion) {
-      toast.default(TOAST_MESSAGE.first.message, { id: TOAST_MESSAGE.first.id });
+      toast.default(SURVEY_TOAST_MESSAGE.first.message, { id: SURVEY_TOAST_MESSAGE.first.id });
+      hasShownFirstToast = true;
       return;
     }
 
     if (isFinalQuestion) {
-      toast.default(TOAST_MESSAGE.final.message, { id: TOAST_MESSAGE.final.id });
+      toast.default(SURVEY_TOAST_MESSAGE.final.message, { id: SURVEY_TOAST_MESSAGE.final.id });
+      hasShownFinalToast = true;
       return;
     }
 
     if (isHalfway) {
-      toast.default(TOAST_MESSAGE.half.message, { id: TOAST_MESSAGE.half.id });
+      toast.default(SURVEY_TOAST_MESSAGE.half.message, { id: SURVEY_TOAST_MESSAGE.half.id });
       hasShownHalfToast = true;
     }
   }, [currentOrder, totalQuestionCount, progressValue]);
---
+++ apps/web/src/constants/surveyMessages.ts
@@ -0,0 +1,18 @@
+export const SURVEY_TOAST_MESSAGE = {
+  first: {
+    id: "survey-toast-first",
+    message: "솔직하게 답변해주세요! 👀",
+  },
+  half: {
+    id: "survey-toast-half",
+    message: "벌써 반이나 응답하셨어요! 조금만 더 힘내요😎",
+  },
+  final: {
+    id: "survey-toast-final",
+    message: "드디어 마지막 질문이에요! 🎁",
+  },
+  error: {
+    id: "survey-toast-error",
+    message: "네트워크에 문제가 발생했어요.\n잠시후 다시 시도해주세요.",
+  },
+} as const;
