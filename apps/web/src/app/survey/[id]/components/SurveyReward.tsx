c6b5dbc837b9aee8399f63536316355990456ace
choi river <130737187+0jenn0@users.noreply.github.com>
2025-11-20 13:39:18 +0900

    feat: 아코디언 디자인 변경 사항 반영하여 수정

--- apps/web/src/app/survey/[id]/components/SurveyReward.tsx
+++ apps/web/src/app/survey/[id]/components/SurveyReward.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { cn } from "@/lib/utils";
 import { Typo } from "@repo/ui/components";
 import { ChevronDown, ChevronUp } from "lucide-react";
 import Image from "next/image";
@@ -80,12 +81,12 @@ export function SurveyReward({
   }, [onVisibilityChange]);
 
   return (
-    <div className="flex w-full flex-col gap-2 rounded-sm bg-white p-3 shadow-[0px_4px_20px_0px_rgba(9,9,11,0.08)]">
+    <div className="flex w-full flex-col rounded-sm bg-white px-3 shadow-[0px_4px_20px_0px_rgba(9,9,11,0.08)]">
       {hasChevron ? (
         <button
           ref={headerButtonRef}
           onClick={() => setIsOpen(!isOpen)}
-          className="flex w-full items-start gap-2"
+          className="flex w-full items-center gap-2 py-3 "
           type="button"
         >
           <div className="flex-1">
@@ -98,7 +99,7 @@ export function SurveyReward({
           </div>
         </button>
       ) : (
-        <div ref={headerDivRef} className="flex w-full items-start gap-2">
+        <div ref={headerDivRef} className="flex w-full items-center gap-2 py-3">
           <div className="flex-1">
             <Typo.Body size="medium" className="text-info text-left">
               설문 리워드
@@ -107,61 +108,29 @@ export function SurveyReward({
         </div>
       )}
 
-      {isOpen ? (
-        <div className="flex w-full flex-col gap-2">
-          {rewardImage && (
-            <div className="flex size-12 shrink-0 items-center justify-center overflow-hidden rounded-full bg-light">
-              <Image
-                src={rewardImage}
-                alt={rewardName}
-                width={32}
-                height={32}
-                className="max-h-8 max-w-8 h-auto w-auto object-contain"
-              />
-            </div>
-          )}
-
-          <div className="flex w-full flex-col gap-1">
-            <Typo.Body size="medium" className="whitespace-pre-line">
-              {rewardName}
+      <div className="flex w-full gap-4 border-t border-default py-3">
+        <div className="flex w-full flex-1 flex-col gap-1">
+          <Typo.Body size="medium" className={cn("whitespace-pre-line", !isOpen && "line-clamp-2")}>
+            {rewardName}
+          </Typo.Body>
+          {isOpen && rewardDescription && (
+            <Typo.Body size="small" className="text-info whitespace-pre-line">
+              * {rewardDescription}
             </Typo.Body>
-            {rewardDescription && (
-              <Typo.Body size="small" className="text-info whitespace-pre-line">
-                * {rewardDescription}
-              </Typo.Body>
-            )}
-          </div>
-        </div>
-      ) : (
-        <div className="flex w-full items-center gap-4">
-          {rewardImage && (
-            <div className="flex size-12 shrink-0 items-center justify-center overflow-hidden rounded-full bg-light">
-              <Image
-                src={rewardImage}
-                alt={rewardName}
-                width={32}
-                height={32}
-                className="max-h-8 max-w-8 h-auto w-auto object-contain"
-              />
-            </div>
           )}
-
-          <div className="flex min-w-0 flex-1 flex-col gap-1">
-            <Typo.Body size="medium" className="line-clamp-1 whitespace-pre-line" ref={titleRef}>
-              {rewardName}
-            </Typo.Body>
-            {rewardDescription && (
-              <Typo.Body
-                size="small"
-                className="text-info line-clamp-1 whitespace-pre-line"
-                ref={descriptionRef}
-              >
-                * {rewardDescription}
-              </Typo.Body>
-            )}
-          </div>
         </div>
-      )}
+        {rewardImage && (
+          <div className="flex size-12 shrink-0 items-center justify-center overflow-hidden rounded-full bg-light">
+            <Image
+              src={rewardImage}
+              alt={rewardName}
+              width={32}
+              height={32}
+              className="max-h-8 max-w-8 h-auto w-auto object-contain"
+            />
+          </div>
+        )}
+      </div>
     </div>
   );
 }
