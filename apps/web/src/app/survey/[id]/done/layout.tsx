ab94d4661054c9b940d74ede7cde0b8333a288d7
choi river <130737187+0jenn0@users.noreply.github.com>
2025-11-24 08:38:53 +0900

    style: 세로 정렬 수정

--- apps/web/src/app/survey/[id]/done/layout.tsx
+++ apps/web/src/app/survey/[id]/done/layout.tsx
@@ -3,7 +3,7 @@ import { ButtonV2, FixedBottomLayout } from "@repo/ui/components";
 export default function SurveyDoneLayout({ children }: { children: React.ReactNode }) {
   return (
     <FixedBottomLayout className="flex flex-col min-h-screen">
-      <div className="w-full flex items-center justify-center flex-1">{children}</div>
+      <div className="w-full flex  justify-center flex-1">{children}</div>
       <FixedBottomLayout.Content className="px-5 py-3">
         <ButtonV2 variant="primary" size="large" className="w-full ">
           <div className="flex justify-center items-center text-center flex-1">확인</div>
