963ffb9ea764374a7fa7934fa4002f49d34a48c3
lurgi <wahoo9040@naver.com>
2025-11-24 14:06:04 +0900

    feat: survey/question 기존 코드 삭제

--- apps/web/src/actions/survey/question/index.ts
+++
@@ -1,5 +0,0 @@
-export { createMultipleChoiceQuestion } from "./create-multiple-choice";
-export { createScaleQuestion } from "./create-scale";
-export { createSubjectiveQuestion } from "./create-subjective";
-
-export * from "./read";
