29d55719188b33b68f9791fa824b98258e2cf9aa
choi river <130737187+0jenn0@users.noreply.github.com>
2025-11-24 10:28:00 +0900

    feat: after 화면 버튼 텍스트 수정 및 메인으로가기 기능 추가

--- apps/web/src/app/survey/[id]/done/layout.tsx
+++ apps/web/src/app/survey/[id]/done/layout.tsx
@@ -1,14 +1,9 @@
-import { ButtonV2, FixedBottomLayout } from "@repo/ui/components";
+import { FixedBottomLayout } from "@repo/ui/components";
 
 export default function SurveyDoneLayout({ children }: { children: React.ReactNode }) {
   return (
     <FixedBottomLayout className="flex flex-col min-h-screen">
-      <div className="w-full flex  justify-center flex-1">{children}</div>
-      <FixedBottomLayout.Content className="px-5 py-3">
-        <ButtonV2 variant="primary" size="large" className="w-full ">
-          <div className="flex justify-center items-center text-center flex-1">확인</div>
-        </ButtonV2>
-      </FixedBottomLayout.Content>
+      <div className="w-full flex justify-center flex-1">{children}</div>
     </FixedBottomLayout>
   );
 }
