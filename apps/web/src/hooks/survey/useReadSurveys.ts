95f1507263677e32aab9363227473f634ee924e1
lurgi <wahoo9040@naver.com>
2025-11-27 13:17:02 +0900

    merge commit

--- apps/web/src/hooks/survey/useReadSurveys.ts
+++ apps/web/src/hooks/survey/useReadSurveys.ts
@@ -1,4 +1,4 @@
-import { getUserSurveys } from "@/actions/survey";
+import { getUserSurveys } from "@/actions/survey/read";
 import { surveySortOrderAtom } from "@/atoms/me/searchAtoms";
 import { surveyQueryKeys } from "@/constants/queryKeys/surveyQueryKeys";
 import { useInfiniteQuery } from "@tanstack/react-query";
