31b7200e11e038a7edddc80906b1dca07c5e27be
lurgi <wahoo9040@naver.com>
2025-12-05 13:59:27 +0900

    chore: devtool 설정

--- apps/web/src/app/admin/components/AdminQueryProvider.tsx
+++ apps/web/src/app/admin/components/AdminQueryProvider.tsx
@@ -2,10 +2,16 @@
 
 import { getAdminQueryClient } from "@/app/admin/lib/get-admin-query-client";
 import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import type { PropsWithChildren } from "react";
 
 export function AdminQueryProvider({ children }: PropsWithChildren) {
   const queryClient = getAdminQueryClient();
 
-  return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
