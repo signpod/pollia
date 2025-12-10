3de7c21ca88268df2948979c79961a260062c7c9
choi river <130737187+0jenn0@users.noreply.github.com>
2025-12-10 18:44:36 +0900

    chore: console.log 삭제

--- apps/web/src/app/mission/[id]/page.tsx
+++ apps/web/src/app/mission/[id]/page.tsx
@@ -3,6 +3,6 @@ import { MissionClientWrapper } from "./MissionClientWrapper";
 
 export default async function MissionPage() {
   const authError = await getAuthError();
-  console.log(authError)
+
   return <MissionClientWrapper initialError={authError} />;
 }
