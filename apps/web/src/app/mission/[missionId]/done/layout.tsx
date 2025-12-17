45c511118d164c273dc209dbfa4ddbd3e73f404c
choi river <130737187+0jenn0@users.noreply.github.com>
2025-12-18 08:30:46 +0900

    [Fix]-미션 완료 레이아웃에 그라디언트 속성 추가

--- apps/web/src/app/mission/[missionId]/done/layout.tsx
+++ apps/web/src/app/mission/[missionId]/done/layout.tsx
@@ -2,7 +2,7 @@ import { FixedBottomLayout } from "@repo/ui/components";
 
 export default function MissionDoneLayout({ children }: { children: React.ReactNode }) {
   return (
-    <FixedBottomLayout className="flex flex-col min-h-screen">
+    <FixedBottomLayout className="flex flex-col min-h-screen" hasGradient>
       <div className="w-full flex justify-center flex-1">{children}</div>
     </FixedBottomLayout>
   );
