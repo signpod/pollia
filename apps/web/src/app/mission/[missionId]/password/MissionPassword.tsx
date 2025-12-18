86e53935cb7a185d2f42ab86ef93ef92d33f77bf
choi river <130737187+0jenn0@users.noreply.github.com>
2025-12-18 13:16:39 +0900

    [Fix]-레이아웃 및 미션 비밀번호 컴포넌트 UI 수정
    * fix: RootLayout에서 min-h-screen을 min-h-svh로 변경하여 화면 비율 조정
    * fix: MissionPassword 컴포넌트에서 min-h-screen을 h-svh로 변경하여 레이아웃 개선

--- apps/web/src/app/mission/[missionId]/password/MissionPassword.tsx
+++ apps/web/src/app/mission/[missionId]/password/MissionPassword.tsx
@@ -23,7 +23,7 @@ export default function MissionPassword() {
   } = useMissionPassword(missionId);
 
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col h-svh overflow-hidden">
       <div className="w-full flex flex-col justify-center flex-1 gap-8 items-center">
         <Typo.MainTitle className="text-center">
           미션 시작을 위한
