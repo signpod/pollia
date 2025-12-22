fc1efcf5ac7229f45bdbe08dc8c277bc7c670088
lurgi <wahoo9040@naver.com>
2025-12-22 15:14:37 +0900

    refactor: LoginModalProvider 제거

--- apps/web/src/app/mission/[missionId]/MissionClientWrapper.tsx
+++ apps/web/src/app/mission/[missionId]/MissionClientWrapper.tsx
@@ -2,7 +2,6 @@
 
 import { FixedBottomLayout, FixedTopLayout } from "@repo/ui/components";
 
-import { LoginModalProvider } from "@/components/providers/LoginModalProvider";
 import { AuthError } from "@/hooks/login/useKakaoLogin";
 import { MissionIntro } from "./MissionIntro";
 
@@ -12,12 +11,10 @@ interface MissionClientWrapperProps {
 
 export function MissionClientWrapper({ initialError }: MissionClientWrapperProps) {
   return (
-    <LoginModalProvider>
-      <FixedBottomLayout className="bg-zinc-700 min-h-screen" hasGradientBlur>
-        <FixedTopLayout>
-          <MissionIntro initialError={initialError} />
-        </FixedTopLayout>
-      </FixedBottomLayout>
-    </LoginModalProvider>
+    <FixedBottomLayout className="bg-zinc-700 min-h-screen" hasGradientBlur>
+      <FixedTopLayout>
+        <MissionIntro initialError={initialError} />
+      </FixedTopLayout>
+    </FixedBottomLayout>
   );
 }
