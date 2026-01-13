5f9757dcef96ac2e0541c07bbb0d41e5bbdf4009
choi river <jaehee@signpod.co.kr>
2026-01-13 12:31:52 +0900

    perf: 정적 페이지 ISR 적용
    - /login 페이지: revalidate=86400 (24시간)
    - /mission/[id]/password 페이지: revalidate=86400 (24시간)
    - 정적 콘텐츠 Edge Cache 활용

--- apps/web/src/app/login/page.tsx
+++ apps/web/src/app/login/page.tsx
@@ -1,6 +1,8 @@
 import { Suspense } from "react";
 import { LoginClient } from "./LoginClient";
 
+export const revalidate = 86400;
+
 export default function LoginPage() {
   return (
     <Suspense>
--- apps/web/src/app/mission/[missionId]/password/page.tsx
+++ apps/web/src/app/mission/[missionId]/password/page.tsx
@@ -1,5 +1,7 @@
 import MissionPassword from "./MissionPassword";
 
+export const revalidate = 86400;
+
 export default function MissionPasswordPage() {
   return <MissionPassword />;
 }
