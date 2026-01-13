88da98fb857031fcdbd079000e3455690aa72196
lurgi <wahoo9040@naver.com>
2026-01-14 08:46:17 +0900

    feat: 불필요한 타입 삭제

---
+++ apps/web/prisma/migrations/20260113234601/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - The values [PRIVACY_CONSENT] on the enum `ActionType` will be removed. If these variants are still used in the database, this will fail.
+
+*/
+-- AlterEnum
+BEGIN;
+CREATE TYPE "ActionType_new" AS ENUM ('MULTIPLE_CHOICE', 'SCALE', 'RATING', 'TAG', 'SUBJECTIVE', 'SHORT_TEXT', 'IMAGE', 'VIDEO', 'PDF', 'DATE', 'TIME');
+ALTER TABLE "actions" ALTER COLUMN "type" TYPE "ActionType_new" USING ("type"::text::"ActionType_new");
+ALTER TYPE "ActionType" RENAME TO "ActionType_old";
+ALTER TYPE "ActionType_new" RENAME TO "ActionType";
+DROP TYPE "public"."ActionType_old";
+COMMIT;
