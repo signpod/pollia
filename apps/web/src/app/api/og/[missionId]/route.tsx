2d9d8b7cd51c384bc8f872317f1d481f8fbccf22
choi river <jaehee@signpod.co.kr>
2026-01-20 07:20:10 +0900

    [Fix]-OG 이미지 생성 코드에 alt 추가

--- apps/web/src/app/api/og/[missionId]/route.tsx
+++ apps/web/src/app/api/og/[missionId]/route.tsx
@@ -11,8 +11,7 @@ async function fetchAndOptimizeImage(
   try {
     const res = await fetch(url, {
       headers: {
-        "User-Agent":
-          "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36",
       },
     });
 
@@ -51,92 +50,88 @@ export async function GET(
     const mission = await missionService.getMission(missionId);
     const { title, imageUrl, brandLogoUrl } = mission;
 
-    const displayTitle = title
-      ? title.length > 50
-        ? `${title.substring(0, 47)}...`
-        : title
-      : "";
+    const displayTitle = title ? (title.length > 50 ? `${title.substring(0, 47)}...` : title) : "";
 
-    // sharp로 이미지 최적화 (PNG 변환)
     const [missionImage, brandLogo, polliaLogo] = await Promise.all([
       imageUrl ? fetchAndOptimizeImage(imageUrl, 630, 630) : null,
       brandLogoUrl ? fetchAndOptimizeImage(brandLogoUrl, 112, 112) : null,
-      fetchAndOptimizeImage(
-        "https://pollia.me/images/pollia-logo.png",
-        undefined,
-        64,
-      ),
+      fetchAndOptimizeImage("https://pollia.me/images/pollia-logo.png", undefined, 64),
     ]);
 
     return new ImageResponse(
-      (
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          backgroundColor: "#fafafa",
+        }}
+      >
+        {/* Left: Mission Image */}
         <div
           style={{
-            width: "100%",
-            height: "100%",
+            width: 630,
+            height: 630,
             display: "flex",
-            backgroundColor: "#fafafa",
+            alignItems: "flex-start",
+            justifyContent: "center",
+            backgroundColor: "#e4e4e7",
+            overflow: "hidden",
           }}
         >
-          {/* Left: Mission Image */}
-          <div
-            style={{
-              width: 630,
-              height: 630,
-              display: "flex",
-              alignItems: "flex-start",
-              justifyContent: "center",
-              backgroundColor: "#e4e4e7",
-              overflow: "hidden",
-            }}
-          >
-            {missionImage && (
-              <img src={missionImage} width={630} height={630} />
-            )}
-          </div>
+          {missionImage && (
+            <img
+              src={missionImage}
+              width={630}
+              height={630}
+              alt="Mission thumbnail"
+              style={{ objectFit: "cover", objectPosition: "top" }}
+            />
+          )}
+        </div>
 
-          {/* Right: Content */}
-          <div
-            style={{
-              flex: 1,
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              padding: 48,
-            }}
-          >
-            {brandLogo && (
-              <img
-                src={brandLogo}
-                width={56}
-                height={56}
-                style={{
-                  borderRadius: 28,
-                  marginBottom: 24,
-                }}
-              />
-            )}
-
-            <div
+        {/* Right: Content */}
+        <div
+          style={{
+            flex: 1,
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            padding: 48,
+          }}
+        >
+          {brandLogo && (
+            <img
+              src={brandLogo}
+              alt="Brand logo"
+              width={56}
+              height={56}
               style={{
-                color: "#09090b",
-                fontSize: 36,
-                fontWeight: 700,
-                lineHeight: 1.3,
+                borderRadius: 28,
                 marginBottom: 24,
               }}
-            >
-              {displayTitle}
-            </div>
+            />
+          )}
 
-            {polliaLogo && (
-              <div style={{ marginTop: "auto", display: "flex" }}>
-                <img src={polliaLogo} height={32} />
-              </div>
-            )}
+          <div
+            style={{
+              color: "#09090b",
+              fontSize: 36,
+              fontWeight: 700,
+              lineHeight: 1.3,
+              marginBottom: 24,
+            }}
+          >
+            {displayTitle}
           </div>
+
+          {polliaLogo && (
+            <div style={{ marginTop: "auto", display: "flex" }}>
+              <img src={polliaLogo} height={32} alt="Pollia logo" />
+            </div>
+          )}
         </div>
-      ),
+      </div>,
       {
         width: 1200,
         height: 630,
