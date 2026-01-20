b0f539dab19c50e57b16561166edbb52de64d2c7
choi river <jaehee@signpod.co.kr>
2026-01-21 07:49:47 +0900

    refactor: MeLayout 컴포넌트 구조 변경 및 헤더 위치 조정

--- apps/web/src/app/me/layout.tsx
+++ apps/web/src/app/me/layout.tsx
@@ -1,19 +1,18 @@
 import { AuthGate } from "@/components/providers/AuthGate";
 import Providers from "@/components/providers/QueryProvider";
 import { ROUTES } from "@/constants/routes";
-import { FixedBottomLayout } from "@repo/ui/components";
 import { MeFooter, MeHeader } from "./components";
 
 export default function MeLayout({ children }: { children: React.ReactNode }) {
   return (
     <AuthGate currentPath={ROUTES.ME}>
       <Providers>
-        <FixedBottomLayout className="bg-background min-h-screen">
-          <MeHeader />
-          {children}
+        <div className="flex flex-col min-h-screen">
+          <div className="flex flex-col flex-1">{children}</div>
           <div className="h-px w-full bg-zinc-100" />
           <MeFooter />
-        </FixedBottomLayout>
+        </div>
+        <MeHeader />
       </Providers>
     </AuthGate>
   );
