612e1d900f0626863d47a5dbf792694ba88d0c70
choi river <jaehee@signpod.co.kr>
2026-01-21 10:12:28 +0900

    [Fix]-마이 페이지 헤더 위치 수정

--- apps/web/src/app/me/layout.tsx
+++ apps/web/src/app/me/layout.tsx
@@ -8,11 +8,11 @@ export default function MeLayout({ children }: { children: React.ReactNode }) {
     <AuthGate currentPath={ROUTES.ME}>
       <Providers>
         <div className="flex flex-col min-h-screen">
+          <MeHeader />
           <div className="flex flex-col flex-1">{children}</div>
           <div className="h-px w-full bg-zinc-100" />
           <MeFooter />
         </div>
-        <MeHeader />
       </Providers>
     </AuthGate>
   );
