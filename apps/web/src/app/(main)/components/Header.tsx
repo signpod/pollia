ab1486af1ac8602d6787f1ff7f0d16ab7488cee5
choi river <jaehee@signpod.co.kr>
2026-02-12 16:09:06 +0900

    feat: 비로그인 좋아요 버튼 클릭 시 로그인 드로어 표시
    - LoginDrawer 공통 컴포넌트 분리 (Header, BottomNavBar 재사용)
    - BottomNavBar에서 비로그인 시 /likes 이동 대신 로그인 드로어 표시
    - Header 로그인 드로어를 공통 컴포넌트로 리팩토링

--- apps/web/src/app/(main)/components/Header.tsx
+++ apps/web/src/app/(main)/components/Header.tsx
@@ -1,17 +1,8 @@
 "use client";
 
 import { ProfileHeader } from "@/components/common/ProfileHeader";
-import { useKakaoLogin } from "@/hooks/login/useKakaoLogin";
-import {
-  ButtonV2,
-  DrawerContent,
-  DrawerProvider,
-  IconButton,
-  KakaoLoginButton,
-  Typo,
-  useDrawer,
-} from "@repo/ui/components";
-import { X } from "lucide-react";
+import { ButtonV2, Typo, useDrawer } from "@repo/ui/components";
+import { LoginDrawer } from "./LoginDrawer";
 
 function LoginDrawerTrigger() {
   const { open } = useDrawer();
@@ -23,38 +14,14 @@ function LoginDrawerTrigger() {
   );
 }
 
-function LoginDrawerContent() {
-  const { close } = useDrawer();
-  const { handleKakaoLogin } = useKakaoLogin();
-
+function HeaderLoginDrawer() {
   return (
-    <DrawerContent className="max-w-[600px]">
-      <div className="relative px-5 pb-5 pt-5">
-        <IconButton icon={X} onClick={close} aria-label="닫기" className="absolute right-5 top-5" />
-        <div className="flex flex-col items-center gap-6 pt-4 pb-2">
-          <Typo.MainTitle size="small" className="whitespace-pre-line text-center">
-            {"카카오 계정으로 로그인 후\n이용할 수 있어요"}
-          </Typo.MainTitle>
-          <KakaoLoginButton
-            inlineIcon
-            onClick={handleKakaoLogin}
-            className="w-full bg-[#FEE500] text-default hover:bg-[#FEE500]/90"
-          />
-        </div>
-      </div>
-    </DrawerContent>
-  );
-}
-
-function LoginDrawer() {
-  return (
-    <DrawerProvider>
+    <LoginDrawer>
       <LoginDrawerTrigger />
-      <LoginDrawerContent />
-    </DrawerProvider>
+    </LoginDrawer>
   );
 }
 
 export function Header() {
-  return <ProfileHeader fallbackRight={<LoginDrawer />} />;
+  return <ProfileHeader fallbackRight={<HeaderLoginDrawer />} />;
 }
